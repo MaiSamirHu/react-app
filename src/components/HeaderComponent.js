44a7772d1fe39fa4e369443f8c576d1ca01fd329
MaiSamirHu <samir.jaiswal@msitjanakpuri.co.in>
2021-08-20 13:38:50 +0530

    login added with uncontrolled form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,20 +1,35 @@
 import React,{Component} from 'react';
-import {Navbar,NavbarBrand,Nav,NavbarToggler,NavItem,Collapse,Jumbotron} from 'reactstrap';
+import {Navbar,NavbarBrand,Nav,NavbarToggler,NavItem,Collapse,Jumbotron,Button,Modal,Form,ModalBody,ModalHeader, FormGroup, Label, Input} from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 class Header extends Component{
     
     constructor(props){
         super(props);
         this.state={
-            isNavOpen :false
+            isNavOpen :false,
+            isModelOpen :false
         };
         this.toggleNav=this.toggleNav.bind(this);
+        this.toggleModel=this.toggleModel.bind(this);
+        this.handleLogin=this.handleLogin.bind(this);
     }
 
     toggleNav(){
         this.setState({
             isNavOpen :!this.state.isNavOpen
         });
+    }
+
+    toggleModel(){
+        this.setState({
+            isModelOpen :!this.state.isModelOpen
+        });
+    }
+
+    handleLogin(event){
+        this.toggleModel();
+        alert("Username:" +this.username.value +"Password :" +this.password.value +"Remember :"+this.remember.checked);
+        event.preventDefault();
     }
 
     render(){
@@ -57,13 +72,19 @@ class Header extends Component{
                     </NavItem>
 
                 </Nav>
-                
+                <Nav className='ml-auto' navbar>
+                <NavItem>
+                    <Button outlint onClick={this.toggleModel}>
+                        <span className='fa fa-sign-in fa-lg'> </span>Login
+                    </Button>
+                </NavItem>
+                </Nav>
                 </Collapse>
             </div>
             </Navbar>
 
             <Jumbotron>
-                <diV className='container'>
+                <div className='container'>
                     <div className='row row-header'>
                         <div className='col-12 col-sm-6'>
                             <h1>
@@ -72,8 +93,31 @@ class Header extends Component{
                             <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                         </div>
                     </div>
-                </diV>
+                </div>
             </Jumbotron>
+
+            <Modal isOpen={this.state.isModelOpen} toggle={this.toggleModal}>
+                <ModalHeader>Login</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                        <FormGroup>
+                            <Label htmlFor="username">Username</Label>
+                            <Input type="text" placeholder="Username" id='username' name='username' innerRef={(input)=>this.username=input}  />
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Label htmlFor="password">Password</Label>
+                            <Input type="password"  placeholder="Password" id='password' name='password'  innerRef={(input)=>this.password=input}/>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                            <Input type="checkbox" name="remember" innerRef={(input)=>this.remember=input}/>Remember Me
+                            </Label>
+                        </FormGroup>
+                        <Button type="submit" value="submit"  color="primary">Login</Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
             
             
         </React.Fragment>

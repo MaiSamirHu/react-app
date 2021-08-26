9a1714e55f2c616c0e55ddb034b934ea8d77136c
MaiSamirHu <samir.jaiswal@msitjanakpuri.co.in>
2021-08-26 14:27:41 +0530

    redux thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,6 +2,7 @@ import { div } from 'prelude-ls';
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {Card,CardImg,CardImgOverlay,CardTitle,Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import {Loading} from './LoadingComponent';
 
 
 
@@ -23,7 +24,7 @@ import {Card,CardImg,CardImgOverlay,CardTitle,Breadcrumb,BreadcrumbItem} from 'r
         }
         
         const Menu=(props)=>{
-            const menu=props.dishes.map((dish)=>{
+            const menu=props.dishes.dishes.map((dish)=>{
                 return(
                 <div key={dish.id}  className='col-12 col-md-5 m-1'>
                     <RenderMenuItems dish={dish} />
@@ -31,39 +32,58 @@ import {Card,CardImg,CardImgOverlay,CardTitle,Breadcrumb,BreadcrumbItem} from 'r
                 
                 );
                 });
-            
-    
-            return(
-                <div className="container">
 
-                    <div className="row">
-
-                        <Breadcrumb>
-
-                            <BreadcrumbItem>
-                                <Link to="/home">Home</Link>
-                            </BreadcrumbItem>
-                            <BreadcrumbItem>
-                                <Link active >Menu</Link>
-                            </BreadcrumbItem>
-                        
-                        </Breadcrumb>
-                        
-                        <div className="col-12">
-                            <h3>Menu</h3>
-                            <hr/>
+                if(props.dishes.isLoading){
+                    return(
+                        <div className="container">
+                            <div className="row">
+                                <Loading/>
+                            </div>
                         </div>
-                    
-                    </div>
+                    );
+                }
+                else if(props.dishes.errMess){
+                    return(
+                        <div className="container">
+                            <div className="row">
+                                <h4>{props.dishes.errMess}</h4>
+                            </div>
+                        </div>
+                    );
+                }
+            
+                else
+                    return(
+                        <div className="container">
 
-                    <div className="row">
-                        {menu}
-                    </div>
+                            <div className="row">
 
-                    
-                    
-                </div>
-            );
+                                <Breadcrumb>
+
+                                    <BreadcrumbItem>
+                                        <Link to="/home">Home</Link>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbItem>
+                                        <Link active >Menu</Link>
+                                    </BreadcrumbItem>
+                                
+                                </Breadcrumb>
+                                
+                                <div className="col-12">
+                                    <h3>Menu</h3>
+                                    <hr/>
+                                </div>
+                            
+                            </div>
+
+                            <div className="row">
+                                {menu}
+                            </div>
+
+                            
+                            
+                        </div>
+                    );
         }
         
 
---
+++ src/redux/reducer.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+export const initialState={
+    dishes :DISHES,
+    comments :COMMENTS,
+    leaders :LEADERS,
+    promotions :PROMOTIONS
+};
+
+export const Reducer=(state=initialState,action)=>{
+    return state;
+};

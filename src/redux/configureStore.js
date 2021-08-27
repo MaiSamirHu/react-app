80d0891c4b97a94059bdda4805ef9d54a75d6af4
MaiSamirHu <samir.jaiswal@msitjanakpuri.co.in>
2021-08-27 12:11:55 +0530

    react redux form revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux';
+import {createForms} from 'react-redux-form';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Leaders } from './leaders';
 import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore=()=>{
     const store=createStore(
@@ -13,8 +15,12 @@ export const ConfigureStore=()=>{
             comments:Comments,
             dishes :Dishes,
             promotions :Promotions,
-            leaders:Leaders
+            leaders:Leaders,
+            ...createForms ({
+                feedback :InitialFeedback
+            })
         }),
+
         applyMiddleware(thunk,logger)
         
     );
